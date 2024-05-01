06bd0a5154ca6072e950f82d826e0f15a4a11c41
aparnasukesh <aparnasukesh0723@gmail.com>
2024-05-01 10:04:49 +0530

    go grpc sample project completed

---
+++ client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+
+	pb "grpc-sample/proto"
+
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect : %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	names := &pb.NamesList{
+		Names: []string{"Akhil", "Aparna", "Athun", "Abu"},
+	}
+
+	callSayHello(client)
+	callSayHelloServerStream(client, names)
+	callSayHelloClientStream(client, names)
+	callSayHelloBidirectionalStream(client, names)
+
+}
